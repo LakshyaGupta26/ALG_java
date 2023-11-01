4538fea20e76e3f3b404fc26dbe9818a109210f1
lakshyagupta <lakshyagupta2602@gmail.com>
2023-11-01 22:11:27 +0530

    Arrays

--- arrays/src/q2.java
+++ arrays/src/q2.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class q2 {
     public static void main(String[] args) {
-        int[] numbers = new int[10];
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 10 numbers:");
+        int[] numbers = new int[10];
 
-        for (int i = 0; i < numbers.length; i++) {
+        System.out.println("Enter 10 numbers:");
+        for (int i = 0; i < 10; i++) {
             numbers[i] = scanner.nextInt();
         }
-//        scanner.close();
+
         System.out.println("Sample Output:");
-        q2(numbers);
-    }
-    public static void q2(int[] numbers) {
         for (int number : numbers) {
             if (number < 0) {
                 System.out.print(number + " ");
             }
-            else if(number >=0){
-                System.out.println(number + " ");
+        }
+        for (int number : numbers) {
+            if (number >= 0) {
+                System.out.print(number + " ");
             }
         }
-        System.out.println();
     }
 }
