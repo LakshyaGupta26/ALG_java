4a0c0099ab81e1355b2b9c0ff867a06b7e9c9dfd
lakshyagupta <lakshyagupta2602@gmail.com>
2023-11-18 12:43:56 +0530

    Overload Functions

--- Functions/src/overload_q1.java
+++ Functions/src/overload_q1.java
@@ -4,30 +4,28 @@ public class overload_q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\nEnter choice: \n1. Check for BUZZ number \n2. Check for Composite number \n3. Exit");
-            int choice = scanner.nextInt();
-            if (choice == 1) {
-                System.out.print("Enter a number: ");
-                int buzzNumber = scanner.nextInt();
-                checkAndPrint(buzzNumber);
-            } else if (choice == 2) {
-                System.out.print("Enter a number: ");
-                int compositeNumber = scanner.nextInt();
-                checkAndPrint(compositeNumber, true);
-            } else if (choice == 3) {
-                System.out.println("Exiting the program. Goodbye!");
-                System.exit(0);
-            } else {
-                System.out.println("Invalid choice. Please enter 1, 2, or 3.");
-            }
+        System.out.println("\nEnter choice: \n1. Check for Buzz number \n2. Check for Composite number \n3. Exit");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.print("Enter a number: ");
+            int buzzNum = scanner.nextInt();
+            check(buzzNum);
+        } else if (choice == 2) {
+            System.out.print("Enter a number: ");
+            int compNum = scanner.nextInt();
+            check(compNum, true);
+        } else if (choice == 3) {
+            System.out.println("Exiting the program. Goodbye!");
+            System.exit(0);
+        } else {
+            System.out.println("Invalid choice.");
         }
     }
-    static void checkAndPrint(int num) {
-        boolean isBuzz = num % 10 == 7 || num % 7 == 0;
-        System.out.println(num + (isBuzz ? " is a BUZZ number." : " is not a BUZZ number."));
+    static void check(int num) {
+        boolean buzz = num % 10 == 7 || num % 7 == 0;
+        System.out.println(num + (buzz ? " is a BUZZ number." : " is not a BUZZ number."));
     }
-    static void checkAndPrint(int num, boolean isComposite) {
+    static void check(int num, boolean isComposite) {
         boolean isComp = false;
         for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
