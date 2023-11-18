c0ae730c04ed7e623f3f6b626cc7c8a9305e4a93
lakshyagupta <lakshyagupta2602@gmail.com>
2023-11-18 12:40:00 +0530

    Functions

--- Functions/src/q4binary.java
+++ Functions/src/q4binary.java
@@ -4,10 +4,14 @@ public class q4binary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        System.out.println("Binary equivalent of " + number + " is " + toBinary(number));
+        int num = scanner.nextInt();
+        System.out.println("Binary eq of " + num + " is " + Number(num));
     }
-    static String toBinary(int num) {
-        return Integer.toBinaryString(num);
+    static String Number(int num) {
+        if (num == 0) {
+            return "0";
+        } else {
+            return Number(num / 2) + num % 2;
+        }
     }
 }
