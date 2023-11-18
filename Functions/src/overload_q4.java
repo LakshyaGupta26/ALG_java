52377e2113fa1c02bf2529ce474d7b5546dd6c8b
lakshyagupta <lakshyagupta2602@gmail.com>
2023-11-18 12:43:17 +0530

    Overload Functions

--- Functions/src/overload_q4.java
+++ Functions/src/overload_q4.java
@@ -4,34 +4,42 @@ public class overload_q4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Enter you choice:\n1. Convert Decimal to Binary :\n2. Convert Binary to Decimal \n3.Exit");
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter a decimal number: ");
-                    int decimalNumber = scanner.nextInt();
-                    System.out.println("Binary equivalent: " + convert(decimalNumber));
-                    break;
-                case 2:
-                    System.out.print("Enter a binary number: ");
-                    String binaryNumber = scanner.next();
-                    System.out.println("Decimal equivalent: " + convert(binaryNumber));
-                    break;
-                case 3:
-                    System.out.println("Exiting the program. Goodbye!");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
-            }
+        System.out.println("Enter your choice:\n1. Convert Decimal to Binary \n2. Convert Binary to Decimal \n3. Exit\n");
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+            System.out.print("Enter a decimal number: ");
+            int decNum = scanner.nextInt();
+            System.out.println("Binary equivalent: " + number(decNum));
+        }
+        else if(choice==2) {
+            System.out.print("Enter a binary number: ");
+            String binNum = scanner.next();
+            System.out.println("Decimal equivalent: " + number(binNum));
+        }
+        else {
+            System.out.println("Exiting program...");
         }
     }
-    static String convert(int decimal) {
-        return Integer.toBinaryString(decimal);
+
+    static String number(int decNum) {
+        if (decNum == 0) {
+            return "0";
+        } else {
+            return number(decNum / 2) + decNum % 2;
+        }
     }
-    static int convert(String binary) {
-        return Integer.parseInt(binary, 2);
+    static int number(String binNum) {
+        int decNum = 0;
+        int pow = 0;
+        for (int i = binNum.length() - 1; i >= 0; i--) {
+            char digit = binNum.charAt(i);
+            if (digit == '1') {
+                decNum += Math.pow(2, pow);
+            } else if (digit != '0') {
+                System.out.println("Enter valid digit");
+            }
+            pow++;
+        }
+        return decNum;
     }
 }
