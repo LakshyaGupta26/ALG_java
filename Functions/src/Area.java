a773a960829f4e16b60279e2a7d9fed62b2a8e2c
lakshyagupta <lakshyagupta2602@gmail.com>
2023-11-18 12:43:33 +0530

    Overload Functions q2

--- Functions/src/Area.java
+++ Functions/src/Area.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class overload_q2 {
-    public static double calculateArea(double base, double height) {
+public class Area {
+    public static double calcArea(double base, double height) {
         return base * height;
     }
-    public static double calculateArea(double d1, double d2, double c) {
+    public static double calcArea(double d1, double d2, double c) {
         return c * d1 * d2;
     }
-    public static double calculateArea(double a, double b, double height_t, double c) {
+    public static double calcArea(double a, double b, double height_t, double c) {
         return c * (a + b) * height_t;
     }
     public static void main(String[] args) {
@@ -24,8 +24,8 @@ public class overload_q2 {
         double b = sc.nextDouble();
         double height_t = sc.nextDouble();
 
-        System.out.println("Area of Parallelogram: " + calculateArea(base, height));
-        System.out.println("Area of Rhombus: " + calculateArea(d1, d2, c));
-        System.out.println("Area of Trapezium: " + calculateArea(a, b, height_t, c));
+        System.out.println("Area of Parallelogram: " + calcArea(base, height));
+        System.out.println("Area of Rhombus: " + calcArea(d1, d2, c));
+        System.out.println("Area of Trapezium: " + calcArea(a, b, height_t, c));
     }
 }
